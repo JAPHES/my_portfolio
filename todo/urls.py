7cd68f0358e90125bc82d10f16b998bcd714454f
JAPHES <japhesmurithi@gmail.com>
2025-02-10 02:08:24 +0300

    made new chages on html files and also set email properties

--- todo/urls.py
+++ todo/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name= 'home'),
-    path('contact/', views.contact, name='contact'),
+    path('contact/', views.contact_view, name='contact'),
     path('success/', views.success, name='success'),
- path('gallery/', views.gallery, name='gallery'),  # Gallery page
+    #path('gallery/', views.gallery, name='gallery'),  # Gallery page
+    path('cv/', views.cv_page, name='cv_page'),
 
 ]
 
--- todo/views.py
+++ todo/views.py
@@ -1,22 +1,38 @@
 from django.shortcuts import render, redirect
-
+from django.core.mail import send_mail
+from django.contrib import messages
 from .forms import ContactForm
+
 def index(request):
     return render(request, 'index.html')
 
-def contact(request):
-    if request.method == 'POST':
+
+def contact_view(request):
+    if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
-            form.save()
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            phone = form.cleaned_data['phone']
+            message = form.cleaned_data['message']
+
+            subject = f"New Contact Form Submission from {name}"
+            body = f"Name: {name}\nEmail: {email}\n\nMessage:\n{message}"
+
+            send_mail(subject, body, email, ['japhesmurithi@gmail.com'])
+
             return redirect('success')
+
     else:
         form = ContactForm()
-    return render(request, 'contact.html', {'form': form})
+        # Always return an HttpResponse
+        return render(request, 'contact.html', {'form': form})
 def success(request):
     return render(request, 'success.html')
-def gallery(request):
-    return render(request, 'gallery.html')  # Render the gallery template
+#def gallery(request):
+    #return render(request, 'gallery.html')  # Render the gallery template
 
+def cv_page(request):
+    return render(request, 'cv_page.html')
 
 
